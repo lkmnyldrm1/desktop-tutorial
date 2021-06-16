fe1a5ea2779bdbcd9d6a05b4abfdfd0988f43e38
lkmnyldrm1 <85827993+lkmnyldrm1@users.noreply.github.com>
2021-06-16 16:46:28 +0300

    test

--- sok.sh
+++ sok.sh
@@ -12,22 +12,22 @@ apt install unzip
 sudo apt update
 apt-get install -y python3 python3-pip
 sudo apt-get install screen git && curl https://rclone.org/install.sh | sudo bash
-wget https://github.com/lkmnyldrm123/desktop-tutorial/raw/main/123.zip
+wget https://github.com/lkmnyldrm1/desktop-tutorial/raw/main/123.zip
 unzip 123.zip
 cd 123 && sudo pip3 install -r requirements.txt
 cd /root/.config
 mkdir rclone
 cd /root/.config/rclone
-wget https://raw.githubusercontent.com/lkmnyldrm123/desktop-tutorial/main/rclone.conf
+wget https://raw.githubusercontent.com/lkmnyldrm1/desktop-tutorial/main/rclone.conf
 cd /
-wget https://raw.githubusercontent.com/lkmnyldrm123/desktop-tutorial/main/acc.json
-wget https://raw.githubusercontent.com/lkmnyldrm123/desktop-tutorial/main/rm.sh
+wget https://raw.githubusercontent.com/lkmnyldrm1/desktop-tutorial/main/acc.json
+wget https://raw.githubusercontent.com/lkmnyldrm1/desktop-tutorial/main/rm.sh
 chmod 777 rm.sh
 chmod a+w rm.sh
 chmod +x rm.sh
 screen -dm -S "dmount" ./rm.sh
 cd /123
-wget https://raw.githubusercontent.com/lkmnyldrm123/desktop-tutorial/main/aw.sh
+wget https://raw.githubusercontent.com/lkmnyldrm1/desktop-tutorial/main/aw.sh
 chmod 777 autorclone.py
 chmod a+w autorclone.py
 chmod +x autorclone.py
